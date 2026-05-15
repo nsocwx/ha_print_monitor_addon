5c57c7a3c5f4d5cb9232ad5fb407bcc060335771
nsocwx <35343508+nsocwx@users.noreply.github.com>
2026-05-14 21:43:45 -0500

    changed container run user

--- ha_print_monitor/app/version.py
+++ ha_print_monitor/app/version.py
@@ -1,6 +1,6 @@
 """Application version metadata."""
 import os
 
-APP_VERSION = os.getenv("APP_VERSION", "0.3.1")
+APP_VERSION = os.getenv("APP_VERSION", "0.3.2")
 BUILD_DATE = os.getenv("BUILD_DATE", "unknown")
 GIT_COMMIT = os.getenv("GIT_COMMIT", "unknown")
